6c322b51a0c15fd9c6331d25fe270a4af2cee7ea
keptan <keptan@protonmail.com>
2021-01-11 20:58:07 -0500

    playing with animated sprites

--- test.cpp
+++ test.cpp
@@ -14,32 +14,6 @@
 
 using namespace SDL2pp;
 
-void sdlTest (void)
-{
-	SDL sdl(SDL_INIT_VIDEO);
-
-	Window window("demo", 
-			SDL_WINDOWPOS_UNDEFINED, 
-			SDL_WINDOWPOS_UNDEFINED,
-			640, 480,
-			SDL_WINDOW_RESIZABLE);
-
-	Renderer rendr (window, -1, SDL_RENDERER_ACCELERATED);
-	Texture ssheet (rendr, DATA_PATH "/lala.png");
-	int vc = rendr.GetOutputHeight() / 2;
-	int hc = rendr.GetOutputWidth() / 2;
-
-	rendr.Clear();
-
-	rendr.Copy(
-			ssheet, 
-			Rect(0, 0, 32, 32), 
-			Rect(hc - 16, vc - 16, 32, 32));
-
-	rendr.Present();
-	SDL_Delay(5000);
-}
-
 
 template <typename F>
 unsigned int time (const F f)
@@ -51,50 +25,13 @@ unsigned int time (const F f)
 	return duration.count();
 };
 
-void benchmark (void)
+struct GameState
 {
-	SparseArray<int, 64000> sparse;
-	std::unordered_map<int, int> regular;
-	std::vector<std::optional<int>> vec;
+	unsigned int frameCount = 0;
+	unsigned int time = 0;
+	unsigned int frameTime = 0;
+};
 
-	const auto time = [&](const auto f)
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		f();
-		auto stop = std::chrono::high_resolution_clock::now();
-		auto duration = duration_cast<std::chrono::milliseconds>(stop - start);
-		std::cout << "function took: " << duration.count() << " milliseconds \n";
-	};
-	std::cout << "random access write, array" << '\n';
-	const auto rArray = [&](){
-		for(int i = 0; i < 2500; i++) regular[std::experimental::randint(0, 63999)] = 1;
-	};
-	time(rArray);
-
-	std::cout << "random access write, sparse array" << '\n';
-	const auto rsArray = [&](){
-		for(int i = 0; i < 2500; i++) sparse.insert(std::experimental::randint(0, 63999), 1);
-	};
-	time(rsArray);
-
-
-	std::cout << "seq read/write, array" << '\n';
-	const auto seqArray = [&](){
-		for(int i = 0; i < 64000; i++)
-		{
-			if(regular[i])
-				regular[i]++;
-		}
-	};
-	time(seqArray);
-
-
-	std::cout << "seq read/write, sparse" << '\n';
-	const auto seqSparse = [&](){
-		for(auto i : sparse) i++;
-	};
-	time(seqSparse);
-}
 
 struct pos 
 {
@@ -113,13 +50,19 @@ struct sprite
 {
 	using StorageStrategy = SparseArray<sprite>;
 	std::shared_ptr<Texture> sheet;
-	int size = 32;
+	int height = 32;
+	int width  = 32;
+	int frames = 0;
+	int frame  = 0;
 };
 
 struct renderTag
 {};
 
-void moveSystem (WorldSystems& world, unsigned int dt)
+struct animationTag
+{};
+
+void moveSystem (WorldSystems& world, GameState& state)
 {
 	auto sig = world.createSignature<pos, velocity>();
 	auto ents = world.signatureScan(sig);
@@ -132,8 +75,8 @@ void moveSystem (WorldSystems& world, unsigned int dt)
 		auto& s  = space->get(i);
 		auto&  v   = vel->get(i);
 
-		s.y += (float(dt) / 1000) * -v.vt * std::cos(v.angle);
-		s.x += (float(dt) / 1000) * v.vt * std::sin(v.angle);
+		s.y += (float(state.frameTime) / 1000) * -v.vt * std::cos(v.angle);
+		s.x += (float(state.frameTime) / 1000) * v.vt * std::sin(v.angle);
 		if(s.x > 640) s.x = -32;
 		if(s.y > 480) s.y = -32;
 		if(s.x < -32) s.x = 640;
@@ -143,7 +86,7 @@ void moveSystem (WorldSystems& world, unsigned int dt)
 	}
 }
 
-void renderSystem (WorldSystems& world, unsigned int dt, Renderer& rendr)
+void renderSystem (WorldSystems& world, GameState& state, Renderer& rendr)
 {
 	auto sig = world.createSignature<pos, sprite, renderTag>();
 	auto ents = world.signatureScan(sig);
@@ -152,14 +95,29 @@ void renderSystem (WorldSystems& world, unsigned int dt, Renderer& rendr)
 	for(const auto i : ents)
 	{
 		auto& space = world.getComponents<pos>()->get(i);
-		auto texture = world.getComponents<sprite>()->get(i);
+		auto& texture = world.getComponents<sprite>()->get(i);
 
-		rendr.Copy(*texture.sheet, Rect(0, 0, texture.size, texture.size), Rect(space.x, space.y, 32, 32));
+		rendr.Copy(*texture.sheet, Rect(0, texture.height * texture.frame, texture.width, texture.height), Rect(space.x, space.y, texture.width, texture.height));
 	
 	}
 	rendr.Present();
 }
 
+void animationSystem (WorldSystems& world, GameState& state)
+{
+	auto sig = world.createSignature<sprite, animationTag>();
+	auto ents = world.signatureScan(sig);
+	for(const auto i : ents)
+	{
+		auto& texture = world.getComponents<sprite>()->get(i);
+		if(!state.frameCount % 30 )
+		{
+			texture.frame = texture.frame + 1;
+
+			if(texture.frame > texture.frames) texture.frame = 0;
+		}
+	}
+}
 
 auto main (void) -> int 
 {
@@ -176,31 +134,38 @@ auto main (void) -> int
 	WorldSystems world;
 	world.registerComponent<pos>();
 	world.registerComponent<renderTag>();
+	world.registerComponent<animationTag>();
 	world.registerComponent<sprite>();
 	world.registerComponent<velocity>();
+
+	auto player = std::make_shared<Texture>(rendr, DATA_PATH "/lala.png");
+	auto fire	= std::make_shared<Texture>(rendr, DATA_PATH "/flame.png");
 
 	for(int i = 0; i < 60; i++)
 	{
 		auto e = world.newEntity();
 		world.addComponent<renderTag>(i, {});
-		world.addComponent<sprite>(i, {std::make_shared<Texture>(rendr, DATA_PATH "/lala.png"), 32});
+		world.addComponent<animationTag>(i, {});
+		world.addComponent<sprite>(i, {fire, 16, 8, 1, 1});
 		world.addComponent<pos>(i, {std::experimental::randint(0, 640), std::experimental::randint(0, 480)});
 		world.addComponent<velocity>(i, {std::experimental::randint(50, 300), std::experimental::randint(0, 0)});
 	}
 
 
-	auto frames = SDL_GetTicks();
-	auto lastFrame = 0;
+	GameState state; 
+	state.time = SDL_GetTicks();
 
 	for(int i = 0; i < 1000; i++)
 	{
 	auto times = SDL_GetTicks();
-	auto dt = times - frames;
-	frames = times;
-	lastFrame = dt;
-	moveSystem(world, dt);
-	renderSystem(world, dt, rendr);
-	SDL_Delay(16 < dt ? 16 : 16 - dt );
+	state.frameTime = times - state.time;
+	state.time = times;
+	moveSystem(world, state);
+	animationSystem(world, state);
+	renderSystem(world, state, rendr);
+
+	state.frameCount++;
+	SDL_Delay(16 < state.frameTime ? 16 : 16 - state.frameTime);
 	}
 
 }
