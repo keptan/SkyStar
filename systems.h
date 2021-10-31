8ed0d516b26c203d56779e84d7f8208c6f0817df
keptan <keptan@protonmail.com>
2021-10-31 19:15:36 -0400

    working edge wrap

--- systems.h
+++ systems.h
@@ -16,32 +16,6 @@
 
 //systems are functions that operate on a subset of entities 
 
-void moveSystem (WorldSystems& world, GameState& state)
-{
-	auto sig = world.createSignature<pos, velocity>();
-	auto ents = world.signatureScan(sig);
-
-	auto space = world.getComponents<pos>();
-	auto vel   = world.getComponents<velocity>();
-
-	for(const auto i : ents)
-	{
-		auto& s  = space->get(i);
-		auto&  dir   = vel->get(i);
-
-		s.x += (double(state.frameTime) / 1000) * (double (dir.dx));
-		s.y += (double(state.frameTime) / 1000) * (double (dir.dy));
-		if(s.x > 640) s.x = -32;
-		if(s.y > 480)
-		{
-			s.x = std::experimental::randint(0, 640);
-			s.y = -32;
-		}
-		if(s.x < -32) s.x = 640;
-		if(s.y < -32) s.y = 480;
-	}
-}
-
 void renderWall (WorldSystems& world, GameState& state, SDL2pp::Renderer& rendr, std::shared_ptr<SDL2pp::Texture> t)
 {
 	const int offset = state.frameCount % 64;
@@ -56,13 +30,11 @@ void renderWall (WorldSystems& world, GameState& state, SDL2pp::Renderer& rendr,
 
 void playerMove (WorldSystems& world, GameState& state)
 {
-	auto sig = world.createSignature<playerTag, velocity>();
+	auto sig = world.createSignature<playerTag, pos>();
 	auto ents = world.signatureScan(sig);
-	auto vel   = world.getComponents<velocity>();
 
 	for(const auto i : ents)
 	{
-		auto& v = vel->get(i);
 		velocity dir{0, 0};
 		if((state.input & InputMask::Up) == InputMask::Up) dir = dir + velocity{0, -50};
 		if((state.input & InputMask::Left) == InputMask::Left) dir = dir + velocity{-50, 0};
@@ -71,9 +43,23 @@ void playerMove (WorldSystems& world, GameState& state)
 
 		dir = dir.normalize(150);
 
-		v = dir;
+		auto& space = world.getComponents<pos>()->get(i);
+		auto& box	= world.getComponents<playerTag>()->get(i);
+
+		auto p = box.body->GetPosition();
+		auto a = box.body->GetAngle();
+		box.body->SetLinearVelocity({dir.dx * 0.04, dir.dy * 0.04});
+
+		space.x = p.x / 0.04;
+		space.y = p.y / 0.04;
+		space.rot = a;
+
+		if(space.y < -32) box.body->SetTransform(b2Vec2(p.x, 488.0f * 0.04f), a);
+		if(space.y > 488) box.body->SetTransform(b2Vec2(p.x, -16.0f * 0.04f), a);
+		if(space.x > 640) box.body->SetTransform(b2Vec2(-8.0f * 0.04f,p.y), a);
+		if(space.x < -8) box.body->SetTransform(b2Vec2(648.0f * 0.04f,p.y), a);
+	
 	}
-	auto space = world.getComponents<pos>();
 }
 
 void pathSystem (WorldSystems& world, GameState& state)
@@ -156,10 +142,12 @@ void boxSystem (WorldSystems& world, GameState& state)
 		space.y = p.y / 0.04;
 		space.rot = a;
 
-		if(space.y > 480) box.body->SetTransform(b2Vec2(p.x, -16.0f * 0.04f), a);
+		if(space.y < -32) box.body->SetTransform(b2Vec2(p.x, 488.0f * 0.04f), a);
+		if(space.y > 488) box.body->SetTransform(b2Vec2(p.x, -16.0f * 0.04f), a);
 		if(space.x > 640) box.body->SetTransform(b2Vec2(-8.0f * 0.04f,p.y), a);
 		if(space.x < -8) box.body->SetTransform(b2Vec2(648.0f * 0.04f,p.y), a);
 	}
+
 }
 
 
