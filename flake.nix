d4d277defab7ca6b3f8001575304b17491ee9224
keptan <keptan@protonmail.com>
2024-12-27 20:04:00 -0500

    nix development env

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "c++23 buildenv";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux"];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+             stdenv = pkgs.gcc14Stdenv;
+          }
+          {
+            packages = with pkgs; [
+	    	cmake
+		pkg-config
+		gdb
+		SDL2
+		SDL2_ttf
+		SDL2_image
+		SDL2.dev
+		SDL2_mixer
+            ];
+
+	    shellHook = '' echo Welcome back commander :3 '';
+          };
+      });
+    };
+}
