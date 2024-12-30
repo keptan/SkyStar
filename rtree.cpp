070fc85236277fac797eb86ecfdf2d83cf1e5919
keptan <keptan@protonmail.com>
2024-12-30 02:47:09 -0500

    fix implicit this invalidation and move quering and balancing into parent class

--- rtree.cpp
+++ rtree.cpp
@@ -49,43 +49,73 @@ struct QNode
 		elements.push_back(e);
 	}
 
-	void balance ( Packed<QNode, size_t>& nodes)
+
+	void rquery (Rectangle& b, Packed<QNode, size_t>& nodes, std::vector< Entity>& collection)
+	{
+		for(const auto& q: elements) 
+		{
+			if( b.collides( q.box)) collection.push_back(q.entity);
+		}
+
+		for(const auto c: children)
+		{
+			if(c == -1) continue;
+			if( b.collides( nodes[c].box)) nodes[c].rquery(b, nodes, collection);
+		}
+	}
+};
+
+//our actual api and datastructure
+struct QTree 
+{
+	Packed<QNode, size_t> nodes;
+	int root;
+
+	QTree (Rectangle r)
+	{
+		root = nodes.create();
+		nodes[root] = QNode{r};
+	}
+
+	void balance (const int n)
 	{
 		//split elements halfway excluding elements that straddle the divide..
-		const int xBoundray = [this]()
+		const int xBoundray = [&]()
 		{
-			std::sort( elements.begin(), elements.end(), 
-					[this](const auto a, const auto b)
+			std::sort( nodes[n].elements.begin(), nodes[n].elements.end(), 
+					[](const auto& a, const auto& b)
 					{
-						return (a.box.corner.x + a.box.w / 2) > (b.box.corner.x + b.box.w /2);
+						return (a.box.corner.x + (a.box.w / 2)) < (b.box.corner.x + (b.box.w /2));
 					});
-			const size_t middle = elements.size() / 2;
-			return elements[middle].box.corner.x + elements[middle].box.w / 2;
+			const size_t middle = nodes[n].elements.size() / 2;
+			return nodes[n].elements[middle].box.corner.x + nodes[n].elements[middle].box.w / 2;
 		}();
-		const int yBoundray =[this]()
+
+		const int yBoundray =[&]()
 		{
-			std::sort( elements.begin(), elements.end(), 
-					[this](const auto a, const auto b)
+			std::sort( nodes[n].elements.begin(), nodes[n].elements.end(), 
+					[](const auto& a, const auto& b)
 					{
-						return (a.box.corner.y + a.box.h / 2) > (b.box.corner.y + b.box.h /2);
+						return (a.box.corner.y + (a.box.h / 2)) < (b.box.corner.y + (b.box.h /2));
 					});
 
-			const size_t middle = elements.size() / 2;
-			return elements[middle].box.corner.y + elements[middle].box.h / 2;
+			const size_t middle = nodes[n].elements.size() / 2;
+			return nodes[n].elements[middle].box.corner.y + nodes[n].elements[middle].box.h / 2;
 		}();
+
 
 		std::array< Rectangle, 4> boxes = 
 		{
-			Rectangle{box.corner,{xBoundray, yBoundray}},
-			Rectangle{{xBoundray, box.corner.y}, {box.corner.x + box.w, yBoundray}},
-			Rectangle{{box.corner.x, yBoundray}, {xBoundray, box.corner.y + box.h}},
-			Rectangle{{xBoundray, yBoundray}, {box.corner.x + box.w, box.corner.y + box.h}}
+			Rectangle{nodes[n].box.corner,{xBoundray, yBoundray}},
+			Rectangle{{xBoundray, nodes[n].box.corner.y}, {nodes[n].box.corner.x + nodes[n].box.w, yBoundray}},
+			Rectangle{{nodes[n].box.corner.x, yBoundray}, {xBoundray, nodes[n].box.corner.y + nodes[n].box.h}},
+Rectangle{{xBoundray, yBoundray}, {nodes[n].box.corner.x + nodes[n].box.w, nodes[n].box.corner.y + nodes[n].box.h}}
 		};
 
 		std::array< std::vector<QElement>,4> boxElements;
 
-		std::vector<QElement> el = elements;
-		elements.clear();
+		std::vector<QElement> el = nodes[n].elements;
+		nodes[n].elements.clear();
 
 		for(const auto& e : el)
 		{
@@ -99,37 +129,43 @@ struct QNode
 							break;
 						}
 			}
-			if(flag) elements.push_back(e);
+			if(flag) nodes[n].elements.push_back(e);
 		}
 
 		for(int i = 0; i < 4; i++)
 		{
 			if(boxElements[i].size())
 			{
-				const auto n = nodes.create();
-				nodes[n] = QNode{boxes[i], std::move( boxElements[i])}; 
-				children[i] = n;
+				const auto pos = nodes.create();
+				nodes[pos] = QNode{boxes[i], std::move( boxElements[i])}; 
+				nodes[n].children[i] = pos;
 			}
 		}
 	}
 
-	void rbalance (Packed<QNode, size_t>& nodes)
+	void rbalance (const int n)
 	{
-		if(elements.size() < 10 || (box.h < 10) || (box.w < 10)) return;
-		balance(nodes);
+		if(nodes[n].elements.size() < 10 || (nodes[n].box.h < 10) || (nodes[n].box.w < 10)) return;
+		balance(n);
 
-		for(const auto c : children) if(c != -1) nodes[c].rbalance(nodes);
+		for(int i = 0; i < 4; i++)
+		{
+			const auto c = nodes[n].children[i];
+			if(c != -1) rbalance(c);
+		}
 	}
 
-};
+	void insert (QElement e)
+	{
+		nodes[root].insert(e);
+	}
 
-//our actual api and datastructure
-struct QTree 
-{
-	//nodes are different to elements because there's a fixed amount in a fixed order
-	//elements will be constantly spawning in and out of existence
-	std::vector<int> nodes;
-	int root;
+	std::vector< Entity> query ( Rectangle& b)
+	{
+		std::vector< Entity> acc;
+		nodes[root].rquery(b, nodes, acc);
+		return acc;
+	}
 
 };
 
@@ -139,7 +175,7 @@ struct hash_pair
     size_t operator()(const std::pair<T1, T2>& p) const
     {
         auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
+auto hash2 = std::hash<T2>{}(p.second);
  
         if (hash1 != hash2) 
 	{
@@ -153,19 +189,20 @@ struct hash_pair
 
 int main (void)
 {
-	Packed<QNode, size_t> nodes;
-	int root = nodes.create();
-	nodes[root] = {{{0,0},{1000,1000}}};
+	QTree tree ( Rectangle{{0,}, {1000,1000}});
 
-	for(int i = 0; i < 1000; i++)
+	for(int i = 0; i < 220; i++)
 	{
-		const int a1 = std::experimental::randint(0, 199);
-		const int a2 = a1 + std::experimental::randint(1, 10);
-		const int b1 = std::experimental::randint(0, 199);
-		const int b2 = b1 + std::experimental::randint(1, 10);
-		const Rectangle r{{a1, b1}, {a2, b2}};
-		nodes[root].insert(r);
+		int x1 = std::experimental::randint(0, 900);
+		int x2 = std::experimental::randint(x1, x1 + 50);
+		int y1 = std::experimental::randint(0, 900);
+		int y2 = std::experimental::randint(y1, y1 + 50);
+
+		tree.insert( QElement{ Rectangle{{x1,y1},{y1,y2}}});
 	}
-	nodes[root].rbalance(nodes);
-	std::cout << nodes.size() << " nodes created" << std::endl;
+
+	tree.rbalance(tree.root);
+	Rectangle query{{0,0},{100,100}};
+	std::cout << tree.query(query).size() << std::endl;
+	std::cout << tree.nodes.size() << std::endl;
 }
