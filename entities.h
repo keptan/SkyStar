e53d9efca5100e8360857fa967253f512448ffc6
keptan <keptan@protonmail.com>
2025-11-02 01:20:24 -0500

    Use sprite in entity for fireball...

--- entities.h
+++ entities.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "components.h"
 #include "systems.h"
 
@@ -9,7 +10,7 @@ Entity player (WorldSystems& world)
 
 	world.addComponent<renderTag>(e, {});
 	world.addComponent<animationTag>(e, {});
-	world.addComponent<pos>(e, {x: xPos, y: yPos});
+	world.addComponent<pos>(e, {xPos, yPos});
 	world.addComponent<playerTag>(e, {});
 	world.addComponent<velocity>(e, {});
 	world.addComponent<Rectangle>(e, {{0,0}, 10, 10});
@@ -21,17 +22,14 @@ Entity fireball (WorldSystems& world, GameState& state)
 {
 	Entity e = world.newEntity();
 
-	auto callback = [](Entity e, WorldSystems& w, GameState& s)
-	{
-		w.killEntity(e);
-	};
-
 	world.addComponent<renderTag>(e, {});
 	world.addComponent<animationTag>(e, {});
 	world.addComponent<outOfBoundsTag>(e, {});
-	world.addComponent<pos>(e, {std::experimental::randint(0, 640), -10});
-	world.addComponent<velocity>(e, {0, std::experimental::randint(90, 150)});
+	world.addComponent<pos>(e, {double (std::experimental::randint(0, 640)), -10});
+	world.addComponent<velocity>(e, {0, double( std::experimental::randint(90, 150))});
 	world.addComponent<Rectangle>(e, {{0,0}, 10, 10});
+	world.addComponent<sprite>(e, {"flame.png",16,8,1,0});
+
 
 
 	return e;
@@ -55,6 +53,7 @@ Entity bolt (WorldSystems& world, GameState& state)
 		world.addComponent<pos>(e, space);
 		world.addComponent<velocity>(e, {0, -250});
 		world.addComponent<Rectangle>(e, {{0,0}, 10, 10});
+		world.addComponent<sprite>(e, {"bolt.png",16,8,1,0});
 	}
 
 	return e;
