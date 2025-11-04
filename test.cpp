d2d5134158e32d5f8d4b35d5b7262d4f6e8ecf63
keptan <keptan@protonmail.com>
2025-11-04 13:48:49 -0500

    testing system graph ordering

--- test.cpp
+++ test.cpp
@@ -12,16 +12,16 @@ auto main (void) -> int
 {
 	Game game;
 	//game.sceneBuild()
-	game.addSystem(sweeper);
+	auto sweep = game.addSystem(sweeper);
 	game.addSystem(playerMove);
 	game.addSystem(pathSystem);
 	game.addSystem(animationSystem);
 	game.addSystem(outOfBounds);
-	game.addSystem(renderSystem);
+	game.addSystem(renderSystem)->before(sweep);
 	game.addSystem(spawnFireBolts);
 	game.addSystem(velocitySystem);
 	game.addSystem(spaceSystem);
-	game.addSystem(collision);
+	game.addSystem(collision)->after(sweep);
 
 	game.addSetup(lalaGameSetup);
 
@@ -32,45 +32,4 @@ auto main (void) -> int
 		int code = game.gameLoop();
 		if (code!= 0) break;
 	}
-}/*
-		if ((state.input & InputMask::PKey) == InputMask::PKey)
-		{
-			for(int i = 0; i < 25; i++)
-			{
-				auto e = fireball(world, state);
-				world.addComponent<sprite>(e, {fire, 16, 8, 1, 0});
-				std::cout << world.eCount() << std::endl;
-			}
-		}
-		if ((state.input & InputMask::Space) == InputMask::Space)
-		{
-			if(state.frameCount % 10 == 0)
-			{
-				auto e = bolt(world, state);
-				world.addComponent<sprite>(e, {lBolt, 16, 8, 1, 0});
-			}
-		}
-		*/
-
-		/*
-		sweeper(world, state);
-		playerMove(world, state);
-		pathSystem(world, state);
-		animationSystem(world, state);
-		renderWall(world, state, window.rendr, wallpaper);
-		renderSystem(world, state, window.rendr);
-		velocitySystem(world, state);
-		outOfBounds(world, state);
-		spaceSystem(world, state, window.rendr, space);
-		collision(world, state, space);
-
-		window.rendr.Present();
-
-		state.frameCount++;
-		const auto endFrame = SDL_GetTicks();
-		const auto frameCost = endFrame - tick;
-		averageFrameTime+= frameCost;
-		SDL_Delay(frameCost >= 7 ? 0 : 7 - frameCost);
-		*/
-
-	//std::cout << averageFrameTime / state.frameCount << '\n';
+}
