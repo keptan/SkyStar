dee88fbd7c691da32abe5bd6d205701f598c7fc4
keptan <keptan@protonmail.com>
2026-03-16 15:55:04 -0400

    Move the camera around, but we will fix the transform mess and make it into vectors isntead

--- systems.h
+++ systems.h
@@ -6,11 +6,10 @@
 #include "geometry.h"
 #include "rtree.h"
 #include <vector>
-
 #include "entities.h"
 
 
-/* we should make this generically rendered in the renderSystem
+/* we should make this generically rendered in the renderSyscastem
 void renderWall (WorldSystems& world, GameState& state, SDL2pp::Renderer& rendr, std::shared_ptr<SDL2pp::Texture> t)
 {
 	const int offset = state.frameCount % 64;
@@ -46,6 +45,91 @@ inline void playerMove (WorldSystems& world, GameState& state, QTree& space, Gra
 	}
 }
 
+inline void playerHexMove (WorldSystems& world, GameState& state, QTree& space, Graphics& graphics)
+{
+	auto sig = world.createSignature<hexPlayerTag, pos, hexTile>();
+	auto ents = world.signatureScan(sig);
+
+
+
+
+	for (const auto i : ents)
+	{
+		long int& cooldown = world.getComponents<hexPlayerTag>()->get(i).coolDown;
+
+
+		const bool left =  ((state.input & InputMask::Left) == InputMask::Left);
+		const bool right = ((state.input & InputMask::Right) == InputMask::Right);
+		const bool up = ((state.input & InputMask::Up) == InputMask::Up);
+
+		if (!left && !right && !up) cooldown = 0;
+
+		if ((state.time - cooldown) < 500)
+		{
+			//std::print("skipping\n");
+			continue;
+		}
+
+		int& frame = world.getComponents<hexTile>()->get(i).type.s.frame;
+		auto& h = world.getComponents<hexTile>()->get(i);
+		auto& p = world.getComponents<pos>()->get(i);
+
+		if (left) frame++;
+		if (right) frame--;
+		if (frame > 7) frame = 0;
+		if (frame < 0) frame = 7;
+
+		if (up && !right && !left)
+		{
+			switch (frame)
+			{
+				case 0: h.hy++; break;
+				case 1: h.hx++, h.hy++;break;
+				case 2: h.hx++; break;
+				case 3: h.hy--, h.hx++; break;
+				case 4: h.hy--; break;
+				case 5: h.hy--; h.hx--; break;
+				case 6: h.hx--; break;
+				case 7: h.hy++; h.hx--; break;
+
+			}
+
+			p = h.tSpace();
+		}
+
+
+
+		if (left || right || up) cooldown = state.time;
+	}
+}
+
+
+inline void cameraMove (WorldSystems& world, GameState& state, QTree& space, Graphics& graphics)
+{
+	if ((state.input & InputMask::UArrow) == InputMask::UArrow) graphics.wy += (500 * state.frameTime) / 1000.0;
+	if ((state.input & InputMask::DArrow) == InputMask::DArrow) graphics.wy -= (500 * state.frameTime) / 1000.0;
+	if ((state.input & InputMask::LArrow) == InputMask::LArrow) graphics.wx += (500 * state.frameTime) / 1000.0;
+	if ((state.input & InputMask::RArrow) == InputMask::RArrow) graphics.wx -= (500 * state.frameTime) / 1000.0;
+	if ((state.input & InputMask::Home) == InputMask::Home)
+	{
+		graphics.wx = 0;
+		graphics.wy = 0;
+		graphics.ws = 1;
+	}
+	if ((state.input & InputMask::ScrollIn) == InputMask::ScrollIn) graphics.ws *= 1.015;
+	if ((state.input & InputMask::ScrollOut) == InputMask::ScrollOut) graphics.ws /= 1.015;
+	graphics.ws = std::clamp(graphics.ws, 0.1, 10.0);
+
+}
+
+/*
+ *what we want to write
+ *auto collect = world.createSignature<path, pos>();
+ *auto space = collect.getComponents<pos>();
+ *for (const auto archetype : space) for (const auto e )
+ *space = archietype.getComponents<pos>()..
+ */
+
 inline void pathSystem (WorldSystems& world, GameState& state, QTree&, Graphics&)
 {
 	auto sig = world.createSignature<path>();
@@ -72,6 +156,35 @@ inline void pathSystem (WorldSystems& world, GameState& state, QTree&, Graphics&
 	}
 }
 
+inline void hexRender (WorldSystems& world, GameState& state, QTree&, Graphics& graphics)
+{
+	auto sig = world.createSignature<pos, hexTile>();
+	auto ents = world.signatureScan(sig);
+
+	std::stable_sort(ents.begin(), ents.end(),
+		[&](auto a, auto b){return world.getComponents<hexTile>()->get(a).hy < world.getComponents<hexTile>()->get(b).hy;});
+
+	std::stable_sort(ents.begin(), ents.end(),
+	[&](auto a, auto b){return world.getComponents<hexTile>()->get(a).hz < world.getComponents<hexTile>()->get(b).hz;});
+
+
+	for(const auto i : ents)
+	{
+		auto& space = world.getComponents<pos>()->get(i);
+		auto& tile = world.getComponents<hexTile>()->get(i);
+		auto& texture = tile.type.s;
+
+		graphics.rendr.Copy
+		(
+			*graphics.getTexture( texture.texture),
+			SDL2pp::Rect(0, texture.height * texture.frame, texture.width, texture.height),
+			SDL2pp::Rect((space.x + graphics.wx + tile.type.tx) * graphics.ws, (space.y + graphics.wy + tile.type.ty) * graphics.ws, texture.width * graphics.ws, texture.height * graphics.ws),
+			space.rot * 180 / M_PI
+		);
+	}
+
+}
+
 inline void renderSystem (WorldSystems& world, GameState& state, QTree&, Graphics& graphics)
 {
 	auto sig = world.createSignature<pos, sprite, renderTag>();
@@ -86,8 +199,8 @@ inline void renderSystem (WorldSystems& world, GameState& state, QTree&, Graphic
 		(
 			*graphics.getTexture( texture.texture),
 			SDL2pp::Rect(0, texture.height * texture.frame, texture.width, texture.height), 
-			SDL2pp::Rect(space.x * 2, space.y * 2, texture.width * 2, texture.height * 2),
-			space.rot * 180 / M_PI	
+			SDL2pp::Rect((space.x + graphics.wx) * graphics.ws, (space.y + graphics.wy) * graphics.ws, texture.width * graphics.ws, texture.height * graphics.ws),
+			space.rot * 180 / M_PI
 		);
 	}
 }
