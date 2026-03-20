182398539ee8ec464e09fee547737ee48d2017e2
keptan <keptan@protonmail.com>
2026-03-19 22:12:34 -0400

    more testing

--- Dtest.cpp
+++ Dtest.cpp
@@ -18,15 +18,25 @@ struct vel
 	int x = 0;
 };
 
+struct bounce
+{
+	int b = 10;
+};
+
 int main (void)
 {
 	World world;
 	const auto s = createSignature<pos, vel>();
 	auto e =0;
 
-	for (int i = 0; i < 25000; i++)
+	for (int i = 0; i < 2000005; i++)
 	{
-		e = world.emplaceEntity(pos{i,i}, vel{0});
+		world.emplaceEntity(pos{i,i}, vel{0});
+	}
+
+	for (int i = 25000; i < 2500001; i++)
+	{
+		world.emplaceEntity(pos{i, i}, bounce{1}, vel {1});
 	}
 
 	//std::print("{}\n", v[0].x);
@@ -34,6 +44,5 @@ int main (void)
 	auto q = World::Query<pos, vel>(world);
 
 	world.each(q, [](const pos& p, const vel& v){std::print("{}\n", p.x);});
-
 	return 0;
 }
